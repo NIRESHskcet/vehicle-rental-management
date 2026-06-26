0def12e346f438b486d59fe245b09a1698e56dd2
NIRESHskcet <727724eucs170@skcet.ac.in>
2026-06-26 09:57:24 +0530

    spring project created

---
+++ src/main/java/com/skcet/vehicle_rental_management/VehicleRentalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.skcet.vehicle_rental_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentalManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skcet/vehicle_rental_management/VehicleRentalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skcet.vehicle_rental_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
