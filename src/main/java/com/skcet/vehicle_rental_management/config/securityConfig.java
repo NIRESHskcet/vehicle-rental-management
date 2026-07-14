48d908000d25f0a58cf1bcb6df3a0e70147724d8
NIRESHskcet <727724eucs170@skcet.ac.in>
2026-07-14 10:36:34 +0530

    security configuration implemented

---
+++ src/main/java/com/skcet/vehicle_rental_management/config/securityConfig.java
@@ -0,0 +1,38 @@
+package com.skcet.vehicle_rental_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class securityConfig {
+    private final JwtAuthenticationFilter jwtFilter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/auth/**",
+                                     "/v3/api-docs/**",
+                                     "/swagger-ui/**",
+                                     "/swagger-ui.html")
+                    .permitAll()
+                    .anyRequest().authenticated())
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtFilter,
+                    UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
